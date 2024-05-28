4a05bcea81dc1405c276b84a19111c2ddb6a1c16
Devin Jaggernauth <neexis@gmail.com>
2024-05-27 21:59:24 -0400

    migrate db to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2024_05_27_103327) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "dentists", force: :cascade do |t|
     t.string "name"
     t.boolean "registration_status"
