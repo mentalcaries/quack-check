42c756db50eb6b9e3eca1a7264039b988abcc0a7
Devin Jaggernauth <neexis@gmail.com>
2024-06-19 08:34:29 -0400

    add names to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable
+
+  validates :first_name, presence: true, length: {minimum: 3, maximum: 25}
+  validates :last_name, presence: true, length: {minimum: 3, maximum: 25}
+
+
 end
